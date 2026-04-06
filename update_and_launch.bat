e02034910ab8e6268b3e3cb0f2c0f470e0b14c56
nl-tools <dylnnue@gmail.com>
2026-04-06 19:41:05 +0200

    launch fix

--- update_and_launch.bat
+++ update_and_launch.bat
@@ -1,16 +1,15 @@
 @echo off
 cd /d "%~dp0"
 
-echo Installing/updating Python dependencies...
-python -m pip install --upgrade pip
-python -m pip install RangeHTTPServer
-
 echo Updating video list...
 python generate_list.py
 
+netstat -ano | findstr :8000 >nul
+if %errorlevel%==0 (
+    echo Server already running
+) else (
+    python -m RangeHTTPServer 8000
+)
+
 echo Starting browser...
-start "" http://localhost:8000
-
-echo Starting video server...
-python -m RangeHTTPServer 8000
-
+start chrome http://localhost:8000
