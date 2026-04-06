60553710ce0224524267b66291da06daa1345115
nl-tools <dylnnue@gmail.com>
2026-04-06 19:11:04 +0200

    launch update

--- update_and_launch.bat
+++ update_and_launch.bat
@@ -3,13 +3,14 @@ cd /d "%~dp0"
 
 echo Installing/updating Python dependencies...
 python -m pip install --upgrade pip
-python -m pip install -r RangeHTTPServer
+python -m pip install RangeHTTPServer
 
 echo Updating video list...
 python generate_list.py
 
+echo Starting browser...
+start "" http://localhost:8000
+
 echo Starting video server...
 python -m RangeHTTPServer 8000
 
-echo Starting browser...
-start "" http://localhost:8000
